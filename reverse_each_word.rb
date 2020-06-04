b2f40b5fbdc0d5eda6009f9e7cc8722eb7c82630
Sarah Peters <sarahpieters15@gmail.com>
2020-06-04 13:26:53 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,4 +1,7 @@
 
-def reverse_each_word(sentence1)
-  sentence1.collect.to_a.reverse.join
+def reverse_each_word(sentence)
+  words_array = sentence.split(" ")
+  new_array = []
+  new_array = words_array.collect {|word| word.reverse}
+  new_array.join(" ")
 end
